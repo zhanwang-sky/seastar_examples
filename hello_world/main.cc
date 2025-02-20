db35d79729921877781cbf3cfb7836935348cd45
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2025-02-20 16:52:25 +0800

    Future & Continuation

--- hello_world/main.cc
+++ hello_world/main.cc
@@ -5,20 +5,41 @@
 //  Created by zhanwang-sky on 2025/2/18.
 //
 
-// g++ main.cc $(pkg-config --libs --cflags --static seastar) -o hello_world
+// c++ main.cc $(pkg-config --libs --cflags --static seastar) -o hello_world
 
 #include <iostream>
 
 #include <seastar/core/app-template.hh>
-#include <seastar/core/reactor.hh>
+#include <seastar/core/future.hh>
+#include <seastar/core/sleep.hh>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+seastar::future<int> slow() {
+  return seastar::sleep(std::chrono::seconds(3)).then([] { return 3; });
+}
+
+seastar::future<int> fast() {
+  return seastar::make_ready_future<int>(0);
+}
+
+seastar::future<> f() {
+  return fast().then([](int val) {
+    cout << "fast Done, val=" << val << endl;
+  });
+}
 
 int main(int argc, char* argv[]) {
   seastar::app_template app;
 
-  app.run(argc, argv, []() {
-    std::cout << "Hello world\n";
-    return seastar::make_ready_future<>();
-  });
+  try {
+    app.run(argc, argv, f);
+  } catch (...) {
+    cerr << "Exception caught: " << std::current_exception() << endl;
+    return 1;
+  }
 
   return 0;
 }
