3ce41851c579ac09667c5108cf60d78eb343a46b
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2025-04-09 18:18:11 +0800

    TCP echo server

--- network.cc
+++ network.cc
@@ -8,19 +8,55 @@
 // c++ network.cc $(pkg-config --libs --cflags --static seastar)
 
 #include <seastar/core/app-template.hh>
+#include <seastar/core/coroutine.hh>
 #include <seastar/core/future.hh>
+#include <seastar/core/reactor.hh>
+#include <seastar/core/when_all.hh>
+#include <seastar/net/api.hh>
 #include <seastar/util/log.hh>
 
 seastar::logger logger("network");
+
+seastar::future<> do_echo(seastar::connected_socket&& conn) {
+  auto remote_addr = conn.remote_address();
+  auto in = conn.input();
+  auto out = conn.output();
+  size_t bytes = 0;
+
+  logger.info("client {} connected", remote_addr);
+
+  for (;;) {
+    auto buf = co_await in.read();
+    if (!buf) {
+      break;
+    }
+    bytes += buf.size();
+    co_await out.write(std::move(buf));
+    co_await out.flush();
+  }
+
+  logger.info("client {} disconnected, {} bytes transferred", remote_addr, bytes);
+}
+
+seastar::future<> tcp_server(uint16_t port) {
+  seastar::listen_options lo;
+  lo.reuse_address = true;
+  auto listener = seastar::listen(seastar::make_ipv4_address({port}), lo);
+  for (;;) {
+    auto res = co_await listener.accept();
+    (void) do_echo(std::move(res.connection));
+  }
+}
+
+seastar::future<> introduce_network_stack() {
+  return seastar::when_all(tcp_server(1234)).discard_result();
+}
 
 int main(int argc, char* argv[]) {
   seastar::app_template app;
 
   try {
-    app.run(argc, argv, [] {
-      logger.info("network");
-      return seastar::make_ready_future<>();
-    });
+    app.run(argc, argv, introduce_network_stack);
   } catch (...) {
     logger.error("Exception caught: {}", std::current_exception());
     return 1;
